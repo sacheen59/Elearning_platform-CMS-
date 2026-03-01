25b57e44d5c78c7612b95a862c985405fb1f22d8
Sachin Barali <baralisachin5925@gmail.com>
2026-03-02 00:10:46 +0545

    completing managing course

--- educa/urls.py
+++ educa/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from courses.views import CourseListView
 
 urlpatterns = [
     path(
@@ -32,7 +33,8 @@ urlpatterns = [
         name='logout'
     ),
     path('admin/', admin.site.urls),
-    path('course/',include('courses.urls'))
+    path('course/',include('courses.urls')),
+    path('', CourseListView.as_view(), name='course_list')
 ]
 
 if settings.DEBUG:
